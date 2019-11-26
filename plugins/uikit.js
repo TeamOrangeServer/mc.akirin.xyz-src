b8e60a5bbce4db787b6a0150359da1359c474dce
sousuke0422 <sousuke20xx@gmail.com>
2019-11-26 20:52:01 +0900

    Icon周りでバグりそう(適当)だったので修正(無効化中)

--- plugins/uikit.js
+++ plugins/uikit.js
@@ -1,7 +1,8 @@
 if (process.browser) {
   const UIkit = require('uikit')
+  const Icons = require('uikit/dist/js/uikit-icons')
   UIkit.use(Icons)
 }
-import Icons from 'uikit/dist/js/uikit-icons'
+//import Icons from 'uikit/dist/js/uikit-icons'
 import 'uikit/dist/css/uikit.css'
 import 'uikit/dist/css/uikit.min.css'
